a75ede746868b1b6627674606cf37a89cbec9da8
Hegl1 <marcel@hegl.at>
2021-03-25 17:41:20 +0100

    Updated makefile and execute script

--- Assignment_03/task1/execute.sh
+++ Assignment_03/task1/execute.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+make > /dev/null
+echo ""
 for i in {1..8}
 do
     ./monte_carlo_threads.out 500000000 $i
@@ -18,3 +20,4 @@ for i in {1..8}
 do
     ./monte_carlo_critical.out 500000000 $i
 done
+make clean /dev/null
