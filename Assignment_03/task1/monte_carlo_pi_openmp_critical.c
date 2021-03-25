1c7ee7fc3bdcfa5e220db4f2f002bb69966cdf23
Hegl1 <marcel@hegl.at>
2021-03-25 13:10:51 +0100

    Added critical monte carlo for task 1

---
+++ Assignment_03/task1/monte_carlo_pi_openmp_critical.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+
+int main(int argc, char** argv){
+
+    int n = 0;
+    int num_threads = 1;
+
+    if(argc < 3){
+        printf("Usage: ./monte_carlo_pi_openmp_critical <number_of_calculations> <number_of_threads>\n");
+        return EXIT_FAILURE;
+    } else{
+        n = atol(argv[1]);
+        num_threads = atol(argv[2]);
+    }
+
+    int seed = 0;
+    long total_inside = 0;
+    omp_set_num_threads(num_threads);
+    double start_time = omp_get_wtime();
+
+    #pragma omp parallel private(seed)
+    {
+        seed = time(NULL);
+        #pragma omp for schedule(static)
+        for(int i = 0; i < n; i++){
+            float x = ((float) rand_r(&seed) / (RAND_MAX));
+            float y = ((float) rand_r(&seed) / (RAND_MAX));
+            if((x*x + y*y)<= 1){
+                #pragma omp critical (summation)
+                {
+                    total_inside++;
+                }
+            }
+        }
+    }
+
+    double end_time = omp_get_wtime();
+    printf("sum: %f time: %2.6f \n", 4 * (float) total_inside / n, end_time-start_time);
+
+
+    return EXIT_SUCCESS;
+}
