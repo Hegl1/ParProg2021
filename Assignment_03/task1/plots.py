a9da2710758649e5b0b55f53a47a92aeeb00e354
Hegl1 <marcel@hegl.at>
2021-03-26 07:55:33 +0100

    updated plots.py and added lcc2 data

--- Assignment_03/task1/plots.py
+++ Assignment_03/task1/plots.py
@@ -1,17 +1,39 @@
 import matplotlib.pyplot as plt
 import sys
 
-def plot_all(threads, red, atomic, critical ,base):
+def plot_total_runtimes(threads, red, atomic, critical ,base):
     plt.plot(base, threads)
+    plt.scatter(base, threads, marker='x')
     plt.plot(base, red)
+    plt.scatter(base, red, marker='x')
     plt.plot(base, atomic)
+    plt.scatter(base, atomic, marker='x')
     plt.plot(base, critical)
+    plt.scatter(base, critical, marker='x')
     plt.xlabel("Number of threads")
     plt.ylabel("Execution time [s]")
     plt.legend(['threads', 'reduced', 'atomic','critical'])
-    #plt.title('Monte Carlo ser/par with 1 to 8 threads')
-    plt.savefig('./plots/monte_carlo.png')
+    plt.title('Runtimes of all parallel versions, 1 to 8 threads')
+    plt.savefig('./plots/absolute_runtimes.png')
     plt.clf()
+
+def plot_delta_runtimes(threads, red, atomic, critical, base):
+    reddiff = [t-e for t, e in zip(threads, red)]
+    atomicdiff = [t-e for t, e in zip(threads, atomic)]
+    criticaldiff = [t-e for t, e in zip(threads, critical)]
+    plt.plot(base, reddiff)
+    plt.scatter(base, reddiff, marker='x')
+    plt.plot(base, atomicdiff)
+    plt.scatter(base, atomicdiff, marker='x')
+    plt.plot(base, criticaldiff)
+    plt.scatter(base, criticaldiff, marker='x')
+    plt.xlabel("Number of threads")
+    plt.ylabel("Execution time delta")
+    plt.legend(['reduced', 'atomic','critical'])
+    plt.title('Relative runtimes OMP-Versions vs Thread-Version')
+    plt.savefig('./plots/relative_runtimes.png')
+    plt.clf()
+
 
 
 def print_table(base, col1, col2, col3, col4, title_line):
@@ -54,5 +76,6 @@ if __name__ == '__main__':
         print("Usage: python3 plots.py <file with formattet output data>")
     else:
         res = readFile(sys.argv[1])
-        plot_all(res[0], res[1], res[2], res[3], range(1,9))
+        plot_total_runtimes(res[0], res[1], res[2], res[3], range(1,9))
+        plot_delta_runtimes(res[0], res[1], res[2], res[3], range(1,9))
         print_table(range(1,9), res[0], res[1], res[2], res[3], '| number_of_threads | thread_version | reduced_version | atomic_version | critical_version |\n')
