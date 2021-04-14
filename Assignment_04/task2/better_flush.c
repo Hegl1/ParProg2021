5b8c74105db44f7aab6d847272fc43a02f809b98
Hegl1 <marcel@hegl.at>
2021-04-14 15:56:41 +0200

    Solved exercise 2

--- Assignment_04/task2/better_flush.c
+++ Assignment_04/task2/better_flush.c
@@ -7,20 +7,21 @@ int main() {
    {
       if (omp_get_thread_num()==0) {
          data = 42;
-         //flush both flag and data to ensure flag write doesnt start before data is in main mem
+         //flush both flag and data to ensure that data gets written before flag
          #pragma omp flush(flag, data) 
          flag = 1;
          //flush flag to ensure that flag is in main memory
          #pragma omp flush(flag)
       }
       else if (omp_get_thread_num()==1) {
-         #pragma omp flush(flag, data) //WTF? 
+         #pragma omp flush(flag, data)
          while (flag < 1) {
-            #pragma omp flush(flag, data) //MORE WTF?
+            //includes data to prevent reordering the for loop after the flush statement in line line 25
+            #pragma omp flush(flag, data)
          }
 
-         //again, wtf? 
-         #pragma omp flush(flag, data)
+         //includes flag to ensure that the flush happens after the while loop
+         #pragma omp flush(data)
          printf("flag=%d data=%d\n", flag, data);
       }
    }
