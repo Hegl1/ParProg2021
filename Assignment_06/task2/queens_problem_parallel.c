b000e0561c2a9869f3b6216f4c72ece9256a1180
TobiH77 <tobihosp@gmail.com>
2021-04-29 15:11:18 +0200

    task2 v1

--- Assignment_06/task2/queens_problem_parallel.c
+++ Assignment_06/task2/queens_problem_parallel.c
@@ -1,96 +1,103 @@
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
-#include <string.h>
+#include <omp.h>
 
-#define IND(y, x) ((y) * (N) + (x))
+// Number of solutions found
+int solutions = 0;
 
-void printMatrix(int N, int *board){
-    for(int i = 1; i <= N; i++) {
-        for(int j = 1; j <= N; j++)
-            printf("%d\t",board[IND(i,j)]);
-        printf("\n");
-    }
-}
-
-int is_attack(int i, int j, int *board, int N) {
-    int k, l;
-
-    // checking for column j
-    for(k = 1; k <= i - 1; k++) {
-        if(board[IND(k,j)] == 1){
-            return 1;
-        }
-    }
-
-    // checking upper right diagonal
-    k = i - 1;
-    l = j + 1;
-
-    while (k >= 1 && l <= N) {
-        if (board[IND(k,l)] == 1){
-            return 1;
-        }
-        k = k + 1;
-        l = l + 1;
-    }
-
-    // checking upper left diagonal
-    k = i - 1;
-    l = j - 1;
-    while (k >= 1 && l >= 1) {
-        if (board[IND(k,l)] == 1){
-            return 1;
-        }
+void placeQ(int queens[], int row, int column, int N, int *localSolution) {
     
-        k = k - 1;
-        l = l - 1;
-    }
-
-  return 0;
-}
-
-int n_queen(int row, int n, int N, int *board) {
-    if (n == 0){
-        return 1;
-    }
-
-    for (int j = 1; j <= N; j++) {
-        if(!is_attack(row, j, board, N)) {
-            board[IND(row,j)] = 1;
-            if (n_queen(row + 1, n - 1, N, board)){
-                return 1;
-            }
-            board[IND(row,j)] = 0; //backtracking
+    for(int i = 0; i < row; i++) {
+        // Vertical check
+        if (queens[i] == column) {
+            return;
+        }
+        
+        // Diagonal check
+        if (abs(queens[i] - column) == (row - i))  {
+            return;
         }
     }
-    return 0;
+    
+    // Set the queen
+    queens[row] = column;
+    
+    //Placed final queen, found a solution
+    if(row == N-1) {
+        (*localSolution)++;
+    }
+    
+    else {
+        // Increment row
+        for(int i = 0; i < N; i++) {
+            placeQ(queens, row + 1, i, N, localSolution);
+        }
+    }
 }
 
-int main(int argc, char **argv) {
+void solve(int N, int *boards[]) {
+    #pragma omp parallel
+    {
+        int localSolution = 0;
+        #pragma omp for schedule(dynamic)
+            for(int i = 0; i < N; i++) {
+                placeQ(boards[i], 0, i, N, &localSolution);
+            }
+        #pragma omp atomic
+            solutions += localSolution;
+    }
+}   
+
+int** alloc_matrix(int m, int n){
+
+    int **p;
+
+    if((p = calloc(m, sizeof(int*)))){
+
+        for(int i = 0; i < m; i++){
+
+            if(!(p[i] = calloc(n, sizeof(int)))){
+
+                printf("\nNo memory for line %d", i);
+                exit(EXIT_FAILURE);
+            }
+        }
+    }else
+    {
+        printf("\nMalloc failed!");
+        exit(EXIT_FAILURE);
+    }
+
+    return p;
+}
+
+void free_matrix(int **mat, int N) { 
+    
+    for (int i = 0; i < N; i++){
+        free(mat[i]);
+    }
+    free(mat);
+}
+
+int main(int argc, char*argv[]) {
     int N = 5;
 
     if (argc > 1) {
         N = atoi(argv[1]);
     }
 
-    int *board =  malloc(sizeof(double) * N * N);
+    int **boards = alloc_matrix(N,N);
+    omp_set_num_threads(8);
 
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++){
-            board[IND(i,j)] = 0;
-        }
-    }
+    double startTime = omp_get_wtime();   
+    solve(N, boards);
+    double endTime = omp_get_wtime();
 
-    int sum = 0;
+    free_matrix(boards, N);
 
-    for(int i = 0; i < N; i++){
-        sum += n_queen(1, N, N, board);
-    }
-
-    printMatrix(N,board);
-    printf("\nSolutions: %d\n", sum);
-
-    free(board);
+    printf("\nNumber of solutions: %d", solutions); 
+    printf("\nExecution time: %2.5f\n", (endTime - startTime));
+    
     return 0;
 }
