300f74a5df37312be0486808aea4e5663806f897
TobiH77 <tobihosp@gmail.com>
2021-05-03 13:24:37 +0200

    add print data

--- Assignment_06/task2/plot.py
+++ Assignment_06/task2/plot.py
@@ -3,7 +3,7 @@ spec = importlib.util.spec_from_file_location("plots_v2.py", "../../plots_v2.py"
 plot = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(plot)
 
-OUT_FILE = 'output.dat'
+OUT_FILE = 'job-print.dat'
 
 plot.plot_runtime_per_thread(OUT_FILE)
 plot.print_table(OUT_FILE)
